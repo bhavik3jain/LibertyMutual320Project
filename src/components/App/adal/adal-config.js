e2da7b921655723e14285500c3d83e1fed0a4a90
Tony Gao <zijiangao@umass.edu>
2016-11-25 03:14:18 -0500

    deleting moved files

--- src/components/App/adal/adal-config.js
+++ src/components/App/adal/adal-config.js
@@ -1,12 +1,15 @@
-var adalConfig = {  
+var mydomain = "https://login.windows.net" + "mydomain"; //replace with LM domain
+
+var adalConfig = {
   tenant: 'common',
-  clientId: '00000000-0000-0000-0000-000000000000',
+  clientId: '00000000-0000-0000-0000-000000000000', //TODO: Replace with properly
+                                                    //generated cleint GUID
   extraQueryParameter: 'nux=1',
   disableRenewal: true,
   endpoints: {
-    'https://graph.microsoft.com': 'https://graph.microsoft.com'
+    mydomain : mydomain
   }
-  // cacheLocation: 'localStorage', // enable this for IE, as sessionStorage does not work for localhost. 
+  // cacheLocation: 'localStorage', // enable this for IE, as sessionStorage does not work for localhost.
 };
 
-module.exports = adalConfig;  
+module.exports = adalConfig;
