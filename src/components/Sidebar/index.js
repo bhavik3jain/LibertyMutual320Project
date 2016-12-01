dcfe59232c5fa234815c490d3f190e0034d1871e
Zach Tousignant <ztous77@gmail.com>
2016-12-01 13:31:19 -0500

    deleted search from sidebar + added css type text_1

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -18,17 +18,7 @@ class Sidebar extends Component {
       <div className="navbar-default sidebar" style={{ marginLeft: '-20px' }} role="navigation">
         <div className="sidebar-nav navbar-collapse collapse">
           <ul className="nav in" id="side-menu">
-            <li className="sidebar-search">
-              <div className="input-group custom-search-form">
-                <input type="text" className="form-control" placeholder="Search..." />
-                <span className="input-group-btn">
-                  <button className="btn btn-default" type="button">
-                    <i className="fa fa-search" />
-                  </button>
-                </span>
-              </div>
-            </li>
-
+            
             <li>
               <a href="" onClick={(e) => { e.preventDefault(); history.push('/'); }} >
                 <i className="fa fa-dashboard fa-fw" /> &nbsp;Home
