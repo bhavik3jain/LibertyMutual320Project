56322f7667dbbe170dd3f2976743e50392d1c05f
Tony Gao <zijiangao@umass.edu>
2016-12-01 14:14:48 -0500

    test POC code up

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -4,6 +4,8 @@ import {
   Panel, PageHeader, ListGroup, ListGroupItem, Button,
 } from 'react-bootstrap';
 
+import { connect_oracle } from '../../components/core/jdbc_connection.js'
+
 
 import s from './Home.css';
 import StatWidget from '../../components/Widget';
@@ -20,7 +22,24 @@ function Home(props, context) {
           <PageHeader>Welcome</PageHeader>
         </div>
       </div>
-
+      <button type="button" class="btn btn-primary" onClick={(e) => {
+          var connection = connect_oracle("admin", "12345678",
+          "thebergdashboard.cx0wblasak2z.us-west-2.rds.amazonaws.com", "1521")
+          connection.execute(
+            "SELECT \'time\' FROM dual"
+            [180],
+            function(err, result)
+            {
+              if (err) {
+                console.error(err.message);
+                doRelease(connection);
+                return;
+              }
+              console.log(result.metaData);
+              alert(result.rows);
+              doRelease(connection);
+            });
+        }}
       <img src={LMlogo} alt="LMlogo" height="90%" width="90%"/>
 
     </div>
